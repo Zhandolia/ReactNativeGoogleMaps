5b332ebf43f868e3c1921e271157230b04822b37
Zhandos <zhandosbrown@gmail.com>
2024-03-07 20:12:24 -0500

    ios maps prep

--- ios/RNTutorial/AppDelegate.mm
+++ ios/RNTutorial/AppDelegate.mm
@@ -1,11 +1,12 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-
+#import <GoogleMaps/GoogleMaps.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyDW9niFBnKabEUdXyBabtRaXUxeLQy9o2k"];
   self.moduleName = @"RNTutorial";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
--- src/screens/googlemaps/index.js
+++ src/screens/googlemaps/index.js
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+   });
 
 export default function GoogleMapsScreen() {
     return (
-        <View>
-            <Text>GoogleMapsScreen</Text>
-        </View>
+        <View style={styles.container}>
+            <MapView
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            region={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+            }}
+            > 
+            </MapView>
+   </View>
     )
 }
