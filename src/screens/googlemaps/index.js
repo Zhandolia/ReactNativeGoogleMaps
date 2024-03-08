bcfbfb80c0ff0e4324e6d03b840b4b54348b1e92
Zhandos <zhandosbrown@gmail.com>
2024-03-07 19:53:17 -0500

    cleaned ts

---
+++ src/screens/googlemaps/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function GoogleMapsScreen() {
+    return (
+        <View>
+            <Text>GoogleMapsScreen</Text>
+        </View>
+    )
+}
